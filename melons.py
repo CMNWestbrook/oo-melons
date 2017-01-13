c5ef332b27943e69f7be7beb9d366b8987fea372
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-12 18:06:02 -0800

    Subclases odd

--- melons.py
+++ melons.py
@@ -4,14 +4,14 @@
 class AbstractMelonOrder(object):
     """ You fill in the rest """
 
-    def __init__(self, species, qty, order_type):
+    def __init__(self, species, qty, order_type, tax=0.0):
         """Initialize melon order attributes"""
 
         self.species = species
         self.qty = qty
         self.shipped = False
         self.order_type = None
-        self.tax = None
+        self.tax = 0.0
 
     def get_total(self):
         """Calculate price."""
@@ -30,16 +30,20 @@ class DomesticMelonOrder(AbstractMelonOrder):
     """ You fill in the rest """
 
     def __init__(self, species, qty):
-        super(DomesticMelonOrder, self).__init__(species, qty, order_type="domestic", tax=0.08)
+        super(DomesticMelonOrder, self).__init__(species, qty, self.order_type, self.tax)
+        self.order_type = "domestic"
+        self.tax = 0.08
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """ You fill in the rest """
 
     def __init__(self, species, qty, country_code):
-        super(InternationalMelonOrder, self).__init__(species, qty, order_type="international", tax=0.17)
+        super(InternationalMelonOrder, self).__init__(species, qty, self.order_type, tax)
+        self.order_type = "international"
+        self.tax = 0.17
 
     def get_country_code(self):
         """Return the country code."""
-
+        # print self.country_code
         return self.country_code
