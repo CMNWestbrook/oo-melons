57d3fc67bb016806ee35c267281c0f56eab0b2eb
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-13 13:05:12 -0800

    Christmas and low quantity working

--- melons.py
+++ melons.py
@@ -4,20 +4,25 @@
 class AbstractMelonOrder(object):
     """ You fill in the rest """
 
-    def __init__(self, species, qty, order_type):
+    def __init__(self, species, qty):
         """Initialize melon order attributes"""
 
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = order_type
-        self.tax = 0.0
+        # self.order_type = order_type
+        # self.tax = 0.0
 
     def get_total(self):
         """Calculate price."""
-
+        if self.species == "Christmas melon":
+            christmas_multiplier = 1.5
+        else:
+            christmas_multiplier = 1.0
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        total = (1 + self.tax) * self.qty * (base_price * christmas_multiplier)
+        if self.qty < 10 and self.order_type == "international":
+            total += 3
         return total
 
     def mark_shipped(self):
@@ -48,3 +53,4 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
         # print self.country_code
         return self.country_code
+
